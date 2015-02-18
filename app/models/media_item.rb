2419cf8dc34108d86341c0f2572f9b25a33d2655
Gautam Naroji <narojigautam@gmail.com>
2015-02-18 21:32:33 +0530

    added validation specs for media item model
    added media item model validations

--- app/models/media_item.rb
+++ app/models/media_item.rb
@@ -1,4 +1,8 @@
 class MediaItem < ActiveRecord::Base
-	
+  validates :title, :presence => true
+  validates :source, :presence => true
+  attr_accessor :user_id
+  validates :user, :presence => true
+
   belongs_to :user, :class_name => "User", :foreign_key => "user_id"
 end
--- spec/models/media_item_spec.rb
+++ spec/models/media_item_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe MediaItem, :type => :model do
   it { should belong_to(:user).class_name('User').with_foreign_key('user_id') }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:source) }
 end
